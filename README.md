9b465fa686f821d63287d9364f5b2f190e8b83c5
NTaylor23 <nicholaswtaylor@proton.me>
2024-11-30 21:35:29 -0800

    day1

---
+++ README.md
@@ -0,0 +1,5 @@
+### Advent Of Code 2024
+
+| Day | Problem                                                   | Solution                                 | Time (s) | Notes |
+| --- | --------------------------------------------------------- | ---------------------------------------- | -------- | ----- |
+| 1   | [Historian Hysteria](https://adventofcode.com/2024/day/1) | [day01.py](advent_of_code_2024/day01.py) | 0.087    |       |
---
+++ day01.py
@@ -0,0 +1,14 @@
+from re import findall
+from collections import Counter
+
+print("---- Day 01 ----")
+
+
+with open(file="input/day01.txt", mode="r", encoding="utf-8") as file:
+    data = [findall(r"\d+", line) for line in file.readlines()]
+    left = [int(pair[0]) for pair in data], 
+    right = [int(pair[-1]) for pair in data]
+    counter = Counter(right)
+    p1 = sum(abs(a - b) for a, b in zip(sorted(left), sorted(right)))
+    p2 = sum(counter.get(a, 0) * a for a in left)
+    print(f"\tPart 1: {p1}\n\tPart 2: {p2}")
