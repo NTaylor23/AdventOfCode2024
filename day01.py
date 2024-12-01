52bf166d48c1ce29e9997ca3b03f72b9222893e3
NTaylor23 <nicholaswtaylor@proton.me>
2024-11-30 21:51:14 -0800

    day1

--- day01.py
+++ day01.py
@@ -1,14 +1,10 @@
 from re import findall
-from collections import Counter
 
 print("---- Day 01 ----")
 
 
 with open(file="input/day01.txt", mode="r", encoding="utf-8") as file:
-    data = [findall(r"\d+", line) for line in file.readlines()]
-    left = [int(pair[0]) for pair in data]
-    right = [int(pair[-1]) for pair in data]
-    counter = Counter(right)
-    p1 = sum(abs(a - b) for a, b in zip(sorted(left), sorted(right)))
-    p2 = sum(counter.get(a, 0) * a for a in left)
-    print(f"\tPart 1: {p1}\n\tPart 2: {p2}")
+    data = [*map(int, findall(r"\d+", file.read()))]
+    l, r = data[0::2], data[1::2]
+    print(f"\tPart 1: {sum(abs(a - b) for a, b in zip(sorted(l), sorted(r)))}")
+    print(f"\tPart 2: {sum(r.count(a) * a for a in l)}")
